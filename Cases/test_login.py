364f5845c7d4aa061333683ac4e46f2b8ce24dd7
sujunzhu <sujunzhu@hotmail.com>
2023-12-07 17:10:44 +0800

    新增测试登录界面的测试用例

--- Cases/test_login.py
+++ Cases/test_login.py
@@ -1,8 +1,9 @@
+
 from Pages.LoginPage import Login
 
-p = Login()
-p.gotos()
-p.inputUser('admin')
-p.inputPassword('admin123')
-result=p.login()
-assert result=='yes'
+def test_login():
+    p = Login()
+    p.gotos()
+    p.inputUser('admin')
+    p.inputPassword('admin123')
+    p.login()
--- main.py
+++
@@ -1,9 +0,0 @@
-from Pages.BasePage import BasePage
-from Pages.LoginPage import Login
-
-if __name__ == '__main__':
-    p=Login()
-    p.gotos()
-    p.inputUser('admin')
-    p.inputPassword('admin123')
-    p.login()
