6251055dd8e847cbf06d5a89d2f5baf6c23e12f7
Sailboats <sujunzhu9@gmail.com>
2023-12-16 10:56:11 +0800

    优化

--- Cases/test_login.py
+++ Cases/test_login.py
@@ -13,26 +13,27 @@ def browser_init():
 
 
 @pytest.fixture(scope='function')
-def setUp(browser_init):
+def setup(browser_init):
     page = browser_init.new_page()
     p = Login(page)
-    p.goto_homepage('http://60.188.248.205:17006')
+    p.goto_homepage('http://127.0.0.1:8080')
+    p.wait(1)
     return p
 
 
 @allure.feature('Login')
 class TestLoginPage:
     #
-    def test_login_without_content(self, setUp):
-        self.p = setUp
+    def test_login_without_content(self, setup):
+        self.p = setup
         self.p.click_login_button()
         locator1 = self.p.check_user_filled()
         assert locator1
         locator2 = self.p.check_password_filled()
         assert locator2
 
-    def test_login_admin(self, setUp):
-        self.p = setUp
+    def test_login_admin(self, setup):
+        self.p = setup
         self.p.inputUser('admin')
         self.p.inputPassword('admin123')
         self.p.click_login_button()
@@ -40,19 +41,19 @@ class TestLoginPage:
         assert '/home/user-center' in self.p.view_page().url
         self.p.click_logout_button()
 
-    def test_login_success(self, setUp):
-        self.p = setUp
+    def test_login_success(self, setup):
+        self.p = setup
         self.p.inputUser('Sailboats')
         self.p.inputPassword('welcome1')
         self.p.click_login_button()
+        self.p.wait(1)
         assert '/home/computer-group' in self.p.view_page().url
         self.p.click_logout_button()
 
-    def test_login_wrong(self, setUp):
-        self.p = setUp
+    def test_login_wrong(self, setup):
+        self.p = setup
         self.p.inputUser('Sailboats')
         self.p.inputPassword('welcome')
         self.p.click_login_button()
-        self.p.wait(1)
         locator = self.p.check_wrong_toast()
         assert locator
